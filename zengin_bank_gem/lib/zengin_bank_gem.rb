c4ec32cca5c1a111a8210ec07e317724f41fc409
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-11-24 10:54:56 +0900

    add enumerable

--- zengin_bank_gem/lib/zengin_bank_gem.rb
+++ zengin_bank_gem/lib/zengin_bank_gem.rb
@@ -1,29 +1,49 @@
 require "zengin_bank_gem/version"
 
-module ZenginBankGem
+module Zengin
+  include Enumerable
   # 銀行
   class Bank
+    include Enumerable
     attr_accessor :code, :name
+    
     def initialize(code, name)
       @code = code
       @name = name
     end
+
+    def each
+
+    end
     # @return[Enumerator] 支店
     def branches
     end
+
   end
 
   # 支店
   class Branch
     attr_accessor :code, :name
+    
     def initialize(code, name)
       @code = code
       @name = name
     end
   end
 
+  def each
+    @zengin.each do |bank|
+      yield bank
+    end
+  end
+
   # @return [Enumerator] 銀行
   def banks
+  end
+end
 
+
+Zengin.banks.each do |bank|
+  bank.branches.each do |branch|
   end
 end
