d80bbfde4ceb385f464e7c8460ae58c8af08c866
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-11-24 10:39:11 +0900

    make skelton

--- zengin_bank_gem/lib/zengin_bank_gem.rb
+++ zengin_bank_gem/lib/zengin_bank_gem.rb
@@ -1,5 +1,29 @@
 require "zengin_bank_gem/version"
 
 module ZenginBankGem
-  # Your code goes here...
+  # 銀行
+  class Bank
+    attr_accessor :code, :name
+    def initialize(code, name)
+      @code = code
+      @name = name
+    end
+    # @return[Enumerator] 支店
+    def branches
+    end
+  end
+
+  # 支店
+  class Branch
+    attr_accessor :code, :name
+    def initialize(code, name)
+      @code = code
+      @name = name
+    end
+  end
+
+  # @return [Enumerator] 銀行
+  def banks
+
+  end
 end
