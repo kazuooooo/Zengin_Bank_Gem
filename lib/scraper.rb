4001183e34a77094b867be99a945a11df81e37e7
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-11-25 10:28:30 +0900

    add process

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,28 +5,68 @@ require 'pry'
 require_relative 'bank'
 
 class Scraper
-    attr_accessor :agent, :page, :home_page
+    attr_accessor :agent, :page, :home_page, :kanas
     def initialize
       @agent = Mechanize.new
       @page = ''
+      @kanas = get_all_kanas
       @home_page = agent.get('http://zengin.ajtw.net/')
     end
-    
-    def process
-      # 1.ホームページにアクセス
-      # 2.金融機関の最初の文字をクリック(あーん)
-      #   a. 金融機関名、金融機関コードを代入 
-      #   b. 支店検索をクリック（その仮名の銀行列分)
-      #     い. 支店名の最初の文字をクリック(あーん)
+
+      # 1.ホームページにアクセス(cache)
+      # 2.金融機関の最初の文字をクリック(あーん)(cache)
+      #   a. 支店検索をクリック（その仮名の銀行列分)
+      #     い. 銀行名と金融機関コードを代入 
+      #     ろ. 支店名の最初の文字をクリック(あーん)
+      #       # get_branch_list_pages
       #       1. リストの列分の支店名、支店コードを代入
+                # get_branches_from_list
       #       2. いに戻る
-      #     ろ. bに戻る
-      #   c. 2.に戻る
+      #     は. bに戻る
+      #   b. 2.に戻る
+    def process
+      # ホームページのリンクにアクセス
+      page = agent.get('http://zengin.ajtw.net/')
+      # きをクリック
+      page = click_link("き")
+      # 一番上の支店をクリック
+      page = click_siten_link
+      # 支店検索ページのきをクリック
+      bank_home_page = page
+      siten_list_pages= []
+      
+      #display_page_info(click_siten_kana_link(bank_home_page, "く"))
+
+      # なぜか途中からおかしくなる。
+      # pageはややこしいから使わなくする
+      # 実行順を要確認
+      kanas.each do |kana|
+        siten_list_page = click_siten_kana_link(bank_home_page,kana)
+        display_page_info(siten_list_page)
+        siten_list_pages << siten_list_page
+      end
+      #page = click_siten_kana_link("き")
+      # 一覧ページをスクレイピング
+      # branches = []
+      # siten_list_pages.each do |list_page|
+      #   kana_branches = get_branches_from_list(list_page)
+      #   branches << kana_branches
+      # end
+    end
+
+    def display_page_info(list_page)
+      info = list_page.search('span.f76')
+      p info.inner_text
+    end
+
+    def click_siten_link
+      form = agent.page.forms[3]
+      button = form.button_with(:value => "支店検索")
+      page = agent.submit(form, button)
     end
 
     # 全ての銀行一覧を取得
     def get_all_banks
-      kanas = get_all_kanas
       banks = []
       kanas.each do |letter|
         banks << get_banks_by_letter(letter)
@@ -68,7 +108,21 @@ class Scraper
       kanas.each do |letter|
         branches << get_branches_by_letter(letter)
       end
-      pp branches
+    end
+
+    def get_branches_from_list(branch_list_page)
+      branches = []
+      tablerows = branch_list_page.search('table.tbl1 tr')
+      # 最初の行はshift
+      tablerows.shift
+      tablerows.each do |tr|
+        name = tr.css('td.g1:first-child').inner_text
+        code = tr.css('td.g2').inner_text
+        branch = Branch.new(code, name)
+        puts branch.code
+        puts branch.name
+        branches << branch
+      end
       branches
     end
 
@@ -92,18 +146,7 @@ class Scraper
       branches
     end
 
-    def test_process(letter_first)
-      # ホームページのリンクにアクセス
-      page = agent.get('http://zengin.ajtw.net/')
-      # きをクリック
-      page = click_link(letter_first)
-      # 一番上の支店をクリック
-      page = click_siten_link
-      # 全ての支店を取得
-      branches = get_all_branches
-    end
 
-    private
     # 指定したpageのurlとvalueからボタンをクリックする
     def click_link(target_value)
       form = home_page.forms[0]
@@ -111,21 +154,23 @@ class Scraper
       page = agent.submit(form, button)
     end
 
-    def click_siten_link
-      form = agent.page.forms[3]
-      button = form.button_with(:value => "支店検索")
-      page = agent.submit(form, button)
+    def click_siten_kana_link(bank_home_page, kana)
+      form = bank_home_page.forms[1]
+      p "click" << kana
+      button = form.button_with(:value => kana)
+      p button
+      list_page = agent.submit(form, button)
     end
 
-    def click_siten_kana_link(target_value)
-      form = agent.page.forms[1]
-      button = form.button_with(:value => target_value)
+    def click_bank_kana_link(kana)
+      form = home_page.forms[1]
+      button = form.button_with(:value => kana)
       page = agent.submit(form, button)
     end
 
     # あ〜んまでのかなを取得
     def get_all_kanas
-      ("ｱ".."ﾝ").to_a.map{ |chr| NKF.nkf("-h1w", NKF.nkf("-Xw", chr)) }
+      kanas = ("ｱ".."ﾜ").to_a.map{ |chr| NKF.nkf("-h1w", NKF.nkf("-Xw", chr)) }
     end
 
 end
