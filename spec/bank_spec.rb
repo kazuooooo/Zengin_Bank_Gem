cb45688301790c76ca8e4137a23be5123fde153e
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-12-01 13:57:44 +0900

    add new bank_spec

---
+++ spec/bank_spec.rb
@@ -0,0 +1,29 @@
+require_relative 'spec_helper'
+require_relative '../lib/bank.rb'
+
+RSpec.describe Bank do
+  
+  attr_accessor :bank
+  before do
+    @bank = Bank.new('0001', 'みずほ銀行')
+  end
+
+  describe '#branches' do
+    it 'branchesを呼んだ時にBranchCollectionが返る' do
+      expect(bank.branches).to be_kind_of(Bank::BranchCollection)
+    end
+  end
+
+  describe Bank::BranchCollection do
+    describe 'ある取得した支店の名前とコードが正しく取得出来ている' do
+      context 'みずほ銀行灘支店の場合' do
+        it '支店コードは491になる' do
+            branch = bank.branches.each.find do |branch|
+                     branch.name == '灘支店'
+                   end
+            expect(branch.branch_code).to eq('491')
+        end
+      end
+    end
+  end
+end
