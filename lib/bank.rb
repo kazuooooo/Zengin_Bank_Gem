0696b023d3dce5dd5b9d32e7cc54dd9206f45533
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-12-09 10:58:08 +0900

    include classes in  module

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,80 +1,84 @@
-class Bank
-  # @!attribute bank_code
-  #   @return [Int] 金融機関コード
-  # @!attribute bank_name
-  #   @return [String] 金融機関名
-  # @!attribute bank_yomi
-  #   @return [String] 金融機関のフリガナ
-  # @!attribute branch_kana_page
-  #   @return [Mechanize::Page] 支店のかな一覧ページ
-  attr_accessor :bank_code, :bank_name, :bank_yomi, :branch_kana_page
-  def initialize(bank_code, bank_name, bank_yomi, branch_kana_page)
-    @bank_code = bank_code
-    @bank_name = bank_name
-    @bank_yomi = bank_yomi
-    @branch_kana_page = branch_kana_page
-  end
+module Zengin
 
-  # EnumerableなBranchCollectionを返す
-  # @return [BankCollection] BankCollection
-  # @example
-  #   Zengin.banks => [BankCollection] 
-  def branches
-    BranchCollection.new(bank_code, bank_name, branch_kana_page)
-  end
-
-  class Branch
+  class Bank
     # @!attribute bank_code
     #   @return [Int] 金融機関コード
     # @!attribute bank_name
     #   @return [String] 金融機関名
-    # @!attribute branch_code
-    #   @return [Int] 支店コード
-    # @!attribute branch_name
-    #   @return [String] 支店名
-    # @!attribute branch_yomi
-    #   @return [String] 支店のフリガナ
-    attr_accessor :bank_name, :bank_code, :branch_code, :branch_name, :branch_yomi
-    def initialize(bank_name, bank_code, branch_code, branch_name, branch_yomi)
-      @bank_name = bank_name
-      @bank_code = bank_code
-      @branch_code = branch_code
-      @branch_name = branch_name
-      @branch_yomi = branch_yomi
-    end
-
-  end
-
-  class BranchCollection
-    include Enumerable
-    # @!attribute bank_code
-    #   @return [Int] 金融機関コード
-    # @!attribute bank_name
-    #   @return [String] 金融機関名
+    # @!attribute bank_yomi
+    #   @return [String] 金融機関のフリガナ
     # @!attribute branch_kana_page
     #   @return [Mechanize::Page] 支店のかな一覧ページ
-    # @!attribute scraper
-    #   @return [Scraper] Scraperインスタンス
-    attr_accessor :bank_code, :bank_name, :branch_kana_page, :scraper
-    def initialize(bank_code, bank_name, branch_kana_page)
+    attr_accessor :bank_code, :bank_name, :bank_yomi, :branch_kana_page
+    def initialize(bank_code, bank_name, bank_yomi, branch_kana_page)
       @bank_code = bank_code
       @bank_name = bank_name
+      @bank_yomi = bank_yomi
       @branch_kana_page = branch_kana_page
-      @scraper = Scraper.instance
     end
 
-    def each
-      return self unless block_given?
-      scraper.get_branch_list_pages(branch_kana_page).each do |page|
-        branches_page_html = Nokogiri::HTML(page.body)
-        branches_page_html.css('table.tbl1 tr:not(:first-child)').each_with_index do |tr, index|
-          branch_name, branch_yomi, branch_code = tr.css('td.g1, td.g2').map{ |td| td.text }
-          next unless branch_name && branch_yomi && branch_code
-          branch = Branch.new(bank_name, bank_code, branch_code, branch_name, branch_yomi)
-          yield branch
+    # EnumerableなBranchCollectionを返す
+    # @return [BankCollection] BankCollection
+    # @example
+    #   Zengin.banks => [BankCollection] 
+    def branches
+      BranchCollection.new(bank_code, bank_name, branch_kana_page)
+    end
+
+    class Branch
+      # @!attribute bank_code
+      #   @return [Int] 金融機関コード
+      # @!attribute bank_name
+      #   @return [String] 金融機関名
+      # @!attribute branch_code
+      #   @return [Int] 支店コード
+      # @!attribute branch_name
+      #   @return [String] 支店名
+      # @!attribute branch_yomi
+      #   @return [String] 支店のフリガナ
+      attr_accessor :bank_name, :bank_code, :branch_code, :branch_name, :branch_yomi
+      def initialize(bank_name, bank_code, branch_code, branch_name, branch_yomi)
+        @bank_name = bank_name
+        @bank_code = bank_code
+        @branch_code = branch_code
+        @branch_name = branch_name
+        @branch_yomi = branch_yomi
+      end
+
+    end
+
+    class BranchCollection
+      include Enumerable
+      # @!attribute bank_code
+      #   @return [Int] 金融機関コード
+      # @!attribute bank_name
+      #   @return [String] 金融機関名
+      # @!attribute branch_kana_page
+      #   @return [Mechanize::Page] 支店のかな一覧ページ
+      # @!attribute scraper
+      #   @return [Scraper] Scraperインスタンス
+      attr_accessor :bank_code, :bank_name, :branch_kana_page, :scraper
+      def initialize(bank_code, bank_name, branch_kana_page)
+        @bank_code = bank_code
+        @bank_name = bank_name
+        @branch_kana_page = branch_kana_page
+        @scraper = Scraper.instance
+      end
+
+      def each
+        return self unless block_given?
+        scraper.get_branch_list_pages(branch_kana_page).each do |page|
+          branches_page_html = Nokogiri::HTML(page.body)
+          branches_page_html.css('table.tbl1 tr:not(:first-child)').each_with_index do |tr, index|
+            branch_name, branch_yomi, branch_code = tr.css('td.g1, td.g2').map{ |td| td.text }
+            next unless branch_name && branch_yomi && branch_code
+            branch = Branch.new(bank_name, bank_code, branch_code, branch_name, branch_yomi)
+            yield branch
+          end
         end
       end
     end
-  end
 
+  end
+  
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,57 +1,58 @@
 require 'singleton'
+module Zengin
+  class Scraper
+    include Singleton
+    # @!attribute agent
+    #   @return [Mechanize] Mechanizeクライアント
+    # @!attribute bank_kana_page
+    #   @return [Mechanize::Page] 銀行のかな一覧ページ
+    # @!attribute banks_list_pages
+    #   @return [Mechanize::Page] 各かなの銀行一覧ページ
+    # @!attribute bank_form
+    #   @return [Mechanize::Form] 各かなボタンが含まれたフォーム
+    attr_accessor :agent, :bank_kana_page, :banks_list_pages, :bank_form
+    def initialize
+      @agent = Mechanize.new
+      @bank_kana_page = agent.get('http://zengin.ajtw.net/')
+      @banks_list_pages = []
+      @bank_form = bank_kana_page.form_with(action: /ginkou.php/)
+      agent.log = Logger.new $stderr
+      agent.keep_alive = false
+    end
 
-class Scraper
-  include Singleton
-  # @!attribute agent
-  #   @return [Mechanize] Mechanizeクライアント
-  # @!attribute bank_kana_page
-  #   @return [Mechanize::Page] 銀行のかな一覧ページ
-  # @!attribute banks_list_pages
-  #   @return [Mechanize::Page] 各かなの銀行一覧ページ
-  # @!attribute bank_form
-  #   @return [Mechanize::Form] 各かなボタンが含まれたフォーム
-  attr_accessor :agent, :bank_kana_page, :banks_list_pages, :bank_form
-  def initialize
-    @agent = Mechanize.new
-    @bank_kana_page = agent.get('http://zengin.ajtw.net/')
-    @banks_list_pages = []
-    @bank_form = bank_kana_page.form_with(action: /ginkou.php/)
-    agent.log = Logger.new $stderr
-    agent.keep_alive = false
-  end
-
-### Bank
-  # 各かなの金融機関一覧ページを取得
-  # @return [Mechanize::Page[]] 各かなの金融機関一覧ページ
-  def get_banks_list_pages
-    bank_form.buttons.map do |initial_kana_button|
-      name, value = [initial_kana_button.name, initial_kana_button.value]
-      bank_form.delete_field!(name)
-      bank_form.add_field!(name, value)
-      begin
-        bank_form.submit
-      rescue Exception => e
-        p "retrying...bank_form.submit"
-        retry
+  ### Bank
+    # 各かなの金融機関一覧ページを取得
+    # @return [Mechanize::Page[]] 各かなの金融機関一覧ページ
+    def get_banks_list_pages
+      bank_form.buttons.map do |initial_kana_button|
+        name, value = [initial_kana_button.name, initial_kana_button.value]
+        bank_form.delete_field!(name)
+        bank_form.add_field!(name, value)
+        begin
+          bank_form.submit
+        rescue Exception => e
+          p "retrying...bank_form.submit"
+          retry
+        end
       end
     end
-  end
 
-### Branch
-  # 引数で与えられた銀行のページから各かなの支店一覧ページを取得
-  # @return [Mechanize::Page[]] 各かなの支店一覧ページ
-  def get_branch_list_pages(branch_kana_page)
-    raise 'ARGMENT MUST BE Mechanize::Page!!' unless branch_kana_page.is_a?(Mechanize::Page)
-    branch_form = branch_kana_page.form_with(action: /shitenmeisai.php/)
-    branch_form.buttons.map do |initial_kana_button|
-      name, value = [initial_kana_button.name, initial_kana_button.value]
-      branch_form.delete_field!(name)
-      branch_form.add_field!(name, value)
-      begin
-        branch_form.submit
-      rescue
-        p "retrying...branch_form.submit"
-        retry
+  ### Branch
+    # 引数で与えられた銀行のページから各かなの支店一覧ページを取得
+    # @return [Mechanize::Page[]] 各かなの支店一覧ページ
+    def get_branch_list_pages(branch_kana_page)
+      raise 'ARGMENT MUST BE Mechanize::Page!!' unless branch_kana_page.is_a?(Mechanize::Page)
+      branch_form = branch_kana_page.form_with(action: /shitenmeisai.php/)
+      branch_form.buttons.map do |initial_kana_button|
+        name, value = [initial_kana_button.name, initial_kana_button.value]
+        branch_form.delete_field!(name)
+        branch_form.add_field!(name, value)
+        begin
+          branch_form.submit
+        rescue
+          p "retrying...branch_form.submit"
+          retry
+        end
       end
     end
   end
